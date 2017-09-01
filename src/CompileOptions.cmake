6190a485f12ad5ba0bebc608ae835a20bedd117c
Jaak Ristioja <jaak.ristioja@cyber.ee>
2017-09-01 16:04:16 +0300

    CompileOptions: Refactored to allow SharemindSet*CompileOptions() take TARGETS.
    If TARGETS are given, it resets the respective COMPILE_DEFINITIONS,
    COMPILE_OPTIONS properties of the given targets. Otherwise, the options are
    added to the current directory (and below) by using ADD_COMPILE_OPTIONS() and
    ADD_DEFINITIONS().

--- src/CompileOptions.cmake
+++ src/CompileOptions.cmake
@@ -23,134 +23,140 @@ SET(SharemindCompileOptions_INCLUDED TRUE)
 
 INCLUDE("${CMAKE_CURRENT_LIST_DIR}/Arguments.cmake")
 INCLUDE("${CMAKE_CURRENT_LIST_DIR}/Lists.cmake")
+INCLUDE("${CMAKE_CURRENT_LIST_DIR}/Polymorphism.cmake")
 INCLUDE(CMakeParseArguments)
 INCLUDE(CheckCCompilerFlag)
 INCLUDE(CheckCXXCompilerFlag)
 
-FUNCTION(SharemindCheckCCompilerFlag flag out)
+FUNCTION(SharemindCheckCompilerFlag compiler flag out)
     STRING(SUBSTRING "${flag}" 1 -1 FlagName)
     STRING(REPLACE "+" "--plus--" FlagName "${FlagName}")
-    CHECK_C_COMPILER_FLAG("${flag}" C_COMPILER_HAS_${FlagName}_FLAG)
-    IF(C_COMPILER_HAS_${FlagName}_FLAG)
-        SET("${out}" TRUE PARENT_SCOPE)
-    ELSE()
-        SET("${out}" FALSE PARENT_SCOPE)
-    ENDIF()
-ENDFUNCTION()
-FUNCTION(SharemindCheckCxxCompilerFlag flag out)
-    STRING(SUBSTRING "${flag}" 1 -1 FlagName)
-    STRING(REPLACE "+" "--plus--" FlagName "${FlagName}")
-    CHECK_CXX_COMPILER_FLAG("${flag}" CXX_COMPILER_HAS_${FlagName}_FLAG)
-    IF(CXX_COMPILER_HAS_${FlagName}_FLAG)
+    SharemindCall("CHECK_${compiler}_COMPILER_FLAG" "${flag}"
+                  "has_flag_${FlagName}")
+    IF("${has_flag_${FlagName}}")
         SET("${out}" TRUE PARENT_SCOPE)
     ELSE()
         SET("${out}" FALSE PARENT_SCOPE)
     ENDIF()
 ENDFUNCTION()
 
-FUNCTION(SharemindCheckAddCCompilerFlag flag)
-    SharemindCheckCCompilerFlag("${flag}" hasFlag)
-    IF("${hasFlag}")
-        ADD_COMPILE_OPTIONS("${flag}")
-    ENDIF()
-ENDFUNCTION()
-FUNCTION(SharemindCheckAddCxxCompilerFlag flag)
-    SharemindCheckCxxCompilerFlag("${flag}" hasFlag)
-    IF("${hasFlag}")
-        ADD_COMPILE_OPTIONS("${flag}")
-    ENDIF()
+FUNCTION(SharemindCheckCompilerFlags compiler out)
+    SharemindNewList(o)
+    FOREACH(flag IN LISTS ARGN)
+        SharemindCheckCompilerFlag("${compiler}" "${flag}" hasFlag)
+        IF("${hasFlag}")
+            SharemindListAppendUnique(o "${flag}")
+        ENDIF()
+    ENDFOREACH()
+    SET("${out}" ${o} PARENT_SCOPE)
 ENDFUNCTION()
 
-FUNCTION(SharemindCheckAddCCompilerFlags)
-    FOREACH(flag IN LISTS ARGN)
-        SharemindCheckAddCCompilerFlag("${flag}")
-    ENDFOREACH()
-ENDFUNCTION()
-FUNCTION(SharemindCheckAddCxxCompilerFlags)
-    FOREACH(flag IN LISTS ARGN)
-        SharemindCheckAddCxxCompilerFlag("${flag}")
-    ENDFOREACH()
-ENDFUNCTION()
+SharemindNewUniqueList(SharemindForcedCompileOptions
+    "-Wall"
+    "-Wextra"
+    "-O2"
+    "$<$<NOT:$<STREQUAL:$<CONFIGURATION>,Release>>:-ggdb>"
+    "$<$<NOT:$<STREQUAL:$<CONFIGURATION>,Release>>:-fno-omit-frame-pointer>"
+    "$<$<STREQUAL:$<CONFIGURATION>,Release>:-DNDEBUG>"
+    "$<$<STREQUAL:$<CONFIGURATION>,Release>:-fomit-frame-pointer>"
+)
+SharemindNewUniqueList(SharemindC99ForcedCompileOptions
+    "-std=c99"
+    ${SharemindForcedCompileOptions}
+)
+SharemindNewUniqueList(SharemindCxx11ForcedCompileOptions
+    "-std=c++11"
+    ${SharemindForcedCompileOptions}
+)
 
-FUNCTION(SharemindSetC99CompileOptions)
+SharemindNewUniqueList(SharemindForcedCompileDefinitions
+    "__STDC_CONSTANT_MACROS"
+    "__STDC_FORMAT_MACROS"
+    "__STDC_LIMIT_MACROS"
+)
+SharemindNewUniqueList(SharemindC99ForcedCompileDefinitions
+    ${SharemindForcedCompileDefinitions}
+)
+SharemindNewUniqueList(SharemindCxx11ForcedCompileDefinitions
+    ${SharemindForcedCompileDefinitions}
+)
+
+SharemindNewUniqueList(SharemindCheckCompileOptions
+    "-Weverything"
+    "-Wfloat-equal"
+    "-Wformat"
+    "-Wlogical-op"
+    "-Wno-packed"
+    "-Wno-padded"
+    "-Wpointer-arith"
+)
+SharemindNewUniqueList(SharemindC99CheckCompileOptions
+    ${SharemindCheckCompileOptions}
+    "-Wabi"
+    "-Wbad-function-cast"
+    "-Wc++-compat"
+    "-Wcast-qual"
+    "-Wconversion"
+    "-Wshadow"
+    "-Wsign-conversion"
+    "-Wstrict-prototypes"
+    "-Wswitch-default"
+    "-Wunused"
+    "-Wunused-macros"
+)
+SharemindNewUniqueList(SharemindCxx11CheckCompileOptions
+    ${SharemindCheckCompileOptions}
+    "-Wno-c++98-compat"
+    "-Wno-c++98-compat-pedantic"
+    "-Wno-covered-switch-default"
+    "-Wno-gnu-case-range"
+    "-Wno-noexcept-type"
+    "-Wno-weak-vtables"
+    "-Wsuggest-override"
+    "-Wzero-as-null-pointer-constant"
+)
+
+FUNCTION(SharemindSetCompileOptions compiler standard)
     SharemindNewList(flags)
     SharemindNewList(opts1)
-    SET(optsn COMPILE_FLAGS DEFINITIONS)
+    SET(optsn TARGETS FORCED_OPTIONS CHECK_OPTIONS DEFINITIONS COMPILE_FLAGS)
     CMAKE_PARSE_ARGUMENTS(CPA "${flags}" "${opts1}" "${optsn}" ${ARGN})
     SharemindCheckNoUnparsedArguments(CPA)
 
-    ADD_COMPILE_OPTIONS(
-        "-std=c99" "-Wall" "-Wextra" "-O2"
-        "$<$<NOT:$<STREQUAL:$<CONFIGURATION>,Release>>:-ggdb>"
-        "$<$<NOT:$<STREQUAL:$<CONFIGURATION>,Release>>:-fno-omit-frame-pointer>"
-        "$<$<STREQUAL:$<CONFIGURATION>,Release>:-DNDEBUG>"
-        "$<$<STREQUAL:$<CONFIGURATION>,Release>:-fomit-frame-pointer>"
+    SharemindNewList(forced
+        ${Sharemind${standard}ForcedCompileOptions}
+        ${CPA_FORCED_OPTIONS}
     )
-    SharemindCheckAddCCompilerFlags(
-        "-Wabi"
-        "-Wbad-function-cast"
-        "-Wc++-compat"
-        "-Wcast-qual"
-        "-Wconversion"
-        "-Weverything"
-        "-Wfloat-equal"
-        "-Wformat"
-        "-Wlogical-op"
-        "-Wno-padded"
-        "-Wpointer-arith"
-        "-Wshadow"
-        "-Wsign-conversion"
-        "-Wstrict-prototypes"
-        "-Wswitch-default"
-        "-Wunused"
-        "-Wunused-macros"
+    SharemindCheckCompilerFlags("${compiler}" optional
+        ${Sharemind${standard}CheckCompileOptions}
+        ${CPA_CHECK_OPTIONS}
         ${CPA_COMPILE_FLAGS}
     )
-    ADD_DEFINITIONS(
-        "-D__STDC_CONSTANT_MACROS"
-        "-D__STDC_FORMAT_MACROS"
-        "-D__STDC_LIMIT_MACROS"
+    SharemindNewList(options ${forced} ${optional})
+    SharemindNewList(definitions
+        ${Sharemind${standard}ForcedCompileDefinitions}
         ${CPA_DEFINITIONS}
     )
-ENDFUNCTION()
 
-FUNCTION(SharemindSetCxx11CompileOptions)
-    SharemindNewList(flags)
-    SharemindNewList(opts1)
-    SET(optsn COMPILE_FLAGS DEFINITIONS)
-    CMAKE_PARSE_ARGUMENTS(CPA "${flags}" "${opts1}" "${optsn}" ${ARGN})
-    SharemindCheckNoUnparsedArguments(CPA)
-
-    ADD_COMPILE_OPTIONS(
-        "-std=c++11" "-Wall" "-Wextra" "-O2"
-        "$<$<NOT:$<STREQUAL:$<CONFIGURATION>,Release>>:-ggdb>"
-        "$<$<NOT:$<STREQUAL:$<CONFIGURATION>,Release>>:-fno-omit-frame-pointer>"
-        "$<$<STREQUAL:$<CONFIGURATION>,Release>:-DNDEBUG>"
-        "$<$<STREQUAL:$<CONFIGURATION>,Release>:-fomit-frame-pointer>"
-    )
-    SharemindCheckAddCxxCompilerFlags(
-        "-Weverything"
-        "-Wlogical-op"
-        "-Wno-c++98-compat"
-        "-Wno-c++98-compat-pedantic"
-        "-Wno-covered-switch-default"
-        "-Wno-float-equal"
-        "-Wno-gnu-case-range"
-        "-Wno-noexcept-type"
-        "-Wno-packed"
-        "-Wno-padded"
-        "-Wno-weak-vtables"
-        "-Wsuggest-override"
-        "-Wzero-as-null-pointer-constant"
-        ${CPA_COMPILE_FLAGS}
-    )
-    ADD_DEFINITIONS(
-        "-D__STDC_CONSTANT_MACROS"
-        "-D__STDC_FORMAT_MACROS"
-        "-D__STDC_LIMIT_MACROS"
-        ${CPA_DEFINITIONS}
-    )
+    IF("${CPA_TARGETS}" STREQUAL "")
+        ADD_COMPILE_OPTIONS(${options})
+        FOREACH(definition IN LISTS definitions)
+            ADD_DEFINITIONS("-D${definition}")
+        ENDFOREACH()
+    ELSE()
+        SET_TARGET_PROPERTIES(${CPA_TARGETS}
+            PROPERTIES
+                COMPILE_OPTIONS "${options}"
+                COMPILE_DEFINITIONS "${definitions}"
+        )
+    ENDIF()
 ENDFUNCTION()
+MACRO(SharemindSetC99CompileOptions)
+    SharemindSetCompileOptions("C" "C99" ${ARGN})
+ENDMACRO()
+MACRO(SharemindSetCxx11CompileOptions)
+    SharemindSetCompileOptions("CXX" "Cxx11" ${ARGN})
+ENDMACRO()
 
 
 ENDIF() # SharemindCompileOptions_INCLUDED
