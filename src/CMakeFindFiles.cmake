f7f6af257830e1f7d3c921773c593e2687e3b006
Jaak Ristioja <jaak.ristioja@cyber.ee>
2017-09-05 11:54:50 +0300

    CMakeFindFiles: Implemented SharemindCreateCMakeFindFiles() to create the files on-the-fly.

--- src/CMakeFindFiles.cmake
+++ src/CMakeFindFiles.cmake
@@ -24,6 +24,7 @@ SET(SharemindCMakeFindFiles_INCLUDED TRUE)
 # TODO: Use CMakePackageConfigHelpers instead?
 
 INCLUDE("${CMAKE_CURRENT_LIST_DIR}/Arguments.cmake")
+INCLUDE("${CMAKE_CURRENT_LIST_DIR}/CMakeHelpersDir.cmake")
 INCLUDE(CMakeParseArguments)
 
 FUNCTION(SharemindUseCMakeFindFiles)
@@ -61,5 +62,44 @@ FUNCTION(SharemindUseCMakeFindFiles)
         COMPONENT "${CPA_COMPONENT}")
 ENDFUNCTION()
 
+FUNCTION(SharemindCreateCMakeFindFiles)
+    SharemindNewList(flags)
+    SET(opts1 PROJECT_NAME COMPONENT VERSION)
+    SET(optsn INCLUDE_DIRS LIBRARIES DEFINITIONS)
+    CMAKE_PARSE_ARGUMENTS(CPA "${flags}" "${opts1}" "${optsn}" ${ARGN})
+    SharemindCheckNoUnparsedArguments(CPA)
+
+    # Handle PROJECT_NAME:
+    IF("${CPA_PROJECT_NAME}" STREQUAL "")
+        SET(CPA_PROJECT_NAME "${CMAKE_PROJECT_NAME}")
+    ENDIF()
+
+    # Handle COMPONENT:
+    IF("${CPA_COMPONENT}" STREQUAL "")
+        SET(CPA_COMPONENT "dev")
+    ENDIF()
+
+    # Handle VERSION:
+    IF("${CPA_VERSION}" STREQUAL "")
+        IF("${PROJECT_VERSION}" STREQUAL "")
+            MESSAGE(FATAL_ERROR
+                    "VERSION not given and variable PROJECT_VERSION is empty!")
+        ENDIF()
+        SET(CPA_VERSION "${PROJECT_VERSION}")
+    ENDIF()
+
+    CONFIGURE_FILE(
+        "${SharemindCMakeHelpersDir}/FindFileConfig.cmake.in"
+        "${CMAKE_CURRENT_BINARY_DIR}/${CPA_PROJECT_NAME}Config.cmake" @ONLY)
+    CONFIGURE_FILE(
+        "${SharemindCMakeHelpersDir}/FindFileConfigVersion.cmake.in"
+        "${CMAKE_CURRENT_BINARY_DIR}/${CPA_PROJECT_NAME}ConfigVersion.cmake"
+        @ONLY)
+    INSTALL(FILES
+        "${CMAKE_CURRENT_BINARY_DIR}/${CPA_PROJECT_NAME}Config.cmake"
+        "${CMAKE_CURRENT_BINARY_DIR}/${CPA_PROJECT_NAME}ConfigVersion.cmake"
+        DESTINATION "lib/${CPA_PROJECT_NAME}"
+        COMPONENT "${CPA_COMPONENT}")
+ENDFUNCTION()
 
 ENDIF() # SharemindCMakeFindFiles_INCLUDED
