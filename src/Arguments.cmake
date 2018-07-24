3289ea9899815ae3ec9cabb9e1217ee732233961
Jaak Ristioja <jaak.ristioja@cyber.ee>
2018-07-24 23:24:17 +0300

    Arguments: Implemented a more generic SharemindCheckArgument() function.

--- src/Arguments.cmake
+++ src/Arguments.cmake
@@ -21,6 +21,8 @@ IF(NOT DEFINED SharemindArguments_INCLUDED)
 SET(SharemindArguments_INCLUDED TRUE)
 
 
+INCLUDE("${CMAKE_CURRENT_LIST_DIR}/Lists.cmake")
+
 FUNCTION(SharemindCheckRequiredArgument prefix argname)
     IF("${${prefix}_${argname}}" STREQUAL "")
         MESSAGE(FATAL_ERROR "Required ${argname} argument not given!")
@@ -31,6 +33,33 @@ FUNCTION(SharemindCheckNoUnparsedArguments prefix)
     IF(DEFINED "${prefix}_UNPARSED_ARGUMENTS")
         MESSAGE(FATAL_ERROR
                 "Unrecognized arguments: ${${prefix}_UNPARSED_ARGUMENTS}")
+    ENDIF()
+ENDFUNCTION()
+
+FUNCTION(SharemindCheckArgument prefix argname)
+    IF(prefix STREQUAL "SharemindCheckArgument")
+        MESSAGE(FATAL_ERROR "Prefix \"SharemindCheckArgument\" not allowed!")
+    ENDIF()
+
+    SET(flags REQUIRED NON_EMPTY)
+    SharemindNewList(opts1)
+    SharemindNewList(optsn)
+    CMAKE_PARSE_ARGUMENTS(SharemindCheckArgument
+                          "${flags}" "${opts1}" "${optsn}" ${ARGN})
+    SharemindCheckNoUnparsedArguments(SharemindCheckArgument)
+
+    SET(varName "${prefix}_${argname}")
+
+    IF(DEFINED SharemindCheckArgument_REQUIRED)
+        IF(NOT DEFINED "${varName}")
+            MESSAGE(FATAL_ERROR "Required ${argname} argument not given!")
+        ENDIF()
+    ENDIF()
+
+    IF(DEFINED SharemindCheckArgument_NON_EMPTY)
+        IF((DEFINED "${varName}") AND ("${${prefix}_${argname}}" STREQUAL ""))
+            MESSAGE(FATAL_ERROR "Argument(s) to ${argname} must not be empty!")
+        ENDIF()
     ENDIF()
 ENDFUNCTION()
 
